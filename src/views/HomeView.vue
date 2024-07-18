9b95d62f9e3b14b28799afd4639377f8a83b3814
Edwin Lin <edwinlin@uitc.com.tw>
2024-07-18 11:35:33 +0800

    feat 新增 總車位數量和可租借的腳踏車數量order按鈕

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,6 +8,8 @@ const bikeList = ref(null);
 const currpage = ref(1);
 const totalpage = ref(1);
 const perpagenumber = 20;
+const sortkey = ref('');
+const sortorder = ref([{ total: 1 }, { available_rent_bikes: 1 }]);
 const tittleList = [
   'sno',
   'sna',
@@ -34,6 +36,16 @@ const filteredData = computed(() => {
     });
   }
 
+  const key = sortkey.value;
+  if (key) {
+    const order = sortorder.value[key];
+    data = data.slice().sort((a, b) => {
+      a = a[key];
+      b = b[key];
+      return (a === b ? 0 : a > b ? 1 : -1) * order;
+    });
+  }
+
   settotalpage(data);
 
   let index = 0;
@@ -46,6 +58,7 @@ const filteredData = computed(() => {
       return false;
     });
   }
+
   return data;
 });
 
@@ -69,6 +82,22 @@ async function callapi() {
 function changepage(page) {
   currpage.value = page;
 }
+function totaldesc() {
+  sortkey.value = 'total';
+  sortorder.value[sortkey.value] = 1;
+}
+function totalasc() {
+  sortkey.value = 'total';
+  sortorder.value[sortkey.value] = -1;
+}
+function abikesdesc() {
+  sortkey.value = 'available_rent_bikes';
+  sortorder.value[sortkey.value] = 1;
+}
+function abikesasc() {
+  sortkey.value = 'available_rent_bikes';
+  sortorder.value[sortkey.value] = -1;
+}
 </script>
 
 <template>
@@ -88,16 +117,78 @@ function changepage(page) {
   ></Paginate>
   <table class="table table-striped">
     <thead>
-      <tr align="center" valign="middle">
-        <th class="col-1 title">站點編號</th>
-        <th class="col-1 title">站點名稱</th>
-        <th class="col-1 title">站點所在區域</th>
-        <th class="col-1 title">站點地址</th>
-        <th class="col-1 title">總車位數量</th>
-        <th class="col-2 title">可租借的腳踏車數量</th>
-        <th class="col-1 title">站點緯度</th>
-        <th class="col-1 title">站點經度</th>
-        <th class="col-1 title">可歸還的腳踏車數量</th>
+      <tr>
+        <th class="col-1 title text-center">站點編號</th>
+        <th class="col-1 title text-center">站點名稱</th>
+        <th class="col-1 title text-center">站點所在區域</th>
+        <th class="col-1 title text-center">站點地址</th>
+        <th class="col-1 title text-center">
+          <span>總車位數量</span>
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-caret-up"
+              viewBox="0 0 16 16"
+              @click="totaldesc"
+            >
+              <path
+                d="M3.204 11h9.592L8 5.519zm-.753-.659 4.796-5.48a1 1 0 0 1 1.506 0l4.796 5.48c.566.647.106 1.659-.753 1.659H3.204a1 1 0 0 1-.753-1.659"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-caret-down"
+              viewBox="0 0 16 16"
+              @click="totalasc"
+            >
+              <path
+                d="M3.204 5h9.592L8 10.481zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659"
+              />
+            </svg>
+          </span>
+        </th>
+        <th class="col-2 title text-center">
+          <div class="flex item-center">
+            <span>可租借的腳踏車數量</span>
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-caret-up"
+                viewBox="0 0 16 16"
+                @click="abikesdesc"
+              >
+                <path
+                  d="M3.204 11h9.592L8 5.519zm-.753-.659 4.796-5.48a1 1 0 0 1 1.506 0l4.796 5.48c.566.647.106 1.659-.753 1.659H3.204a1 1 0 0 1-.753-1.659"
+                />
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-caret-down"
+                viewBox="0 0 16 16"
+                @click="abikesasc"
+              >
+                <path
+                  d="M3.204 5h9.592L8 10.481zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659"
+                />
+              </svg>
+            </span>
+          </div>
+        </th>
+        <th class="col-1 title text-center">站點緯度</th>
+        <th class="col-1 title text-center">站點經度</th>
+        <th class="col-1 title text-center">可歸還的腳踏車數量</th>
       </tr>
     </thead>
     <tbody>
@@ -113,7 +204,8 @@ function changepage(page) {
 <style>
 .title {
   border: solid black 1px;
-  height: 20px;
+  height: 50px;
   background-color: aqua;
+  vertical-align: middle;
 }
 </style>
