bcf498bfde989c839eb92752e05dfb95a4dbc036
Edwin Lin <edwinlin@uitc.com.tw>
2024-07-19 15:54:41 +0800

    feat home use app.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    }
-  ]
-})
+  routes: []
+});
 
-export default router
+export default router;
